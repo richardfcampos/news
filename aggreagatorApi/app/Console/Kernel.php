4b7b07b5a3afda93771e5963bc3e4da1cf5b75ea
Richard F Campos <richardfcampos@gmail.com>
2025-01-15 00:15:32 -0300

    feat: add scheduler

--- aggreagatorApi/app/Console/Kernel.php
+++ aggreagatorApi/app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(FetchArticles::class)->hourly();
+        $schedule->command(FetchArticles::class)->everyMinute();
     }
 
     protected function commands()
--- aggreagatorApi/routes/console.php
+++ aggreagatorApi/routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\FetchArticles;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+//just for testing purposes we are adding everyMinute
+Schedule::command(FetchArticles::class)->everyMinute();
